cb0191ffd71ea4932a07fbdbda73d036212d28bf
ncip48 <mbahcip00@gmail.com>
2026-07-11 09:14:10 +0700

    remove unused

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
 import re
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from statsmodels.stats.diagnostic import het_breuschpagan
-from scipy.stats import shapiro, jarque_bera
+from scipy.stats import shapiro
 
 def clean_data(df):
     def parse_price(x):
